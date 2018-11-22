2f2a471003817df693dccceb24e6464fdf523693
Peter Kassenaar <info@kassenaar.com>
2018-11-22 09:22:47 +0100

    Google Maps project met startcomponent toegevoegd

---
+++ maps-met-startcomponent/js/app.js
@@ -0,0 +1,21 @@
+// app.js
+var app = new Framework7({
+    root: '#app', // <-- Verwijst naar <div id="app">
+    name: 'Google Maps project', // <-- Naam van de applicatie
+    id: 'com.mindef.project02', // <-- naam van de bundle, voor publicatie in App Stores
+    routes: [
+        // <-- Routes/pagina's binnen de app
+        {
+            path: '',
+            componentUrl: './pages/maps-component.html'
+        },
+        {
+            path: '/maps-component/',
+            componentUrl: './pages/maps-component.html'
+        }
+    ]
+    // <-- overige parameters
+});
+
+// Elke app heeft een main view, verwijst naar <div class="view view-main">
+var mainView = app.views.create('.view-main');
